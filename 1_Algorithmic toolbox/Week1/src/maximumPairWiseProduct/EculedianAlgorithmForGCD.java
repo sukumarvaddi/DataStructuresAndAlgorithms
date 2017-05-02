bd7a532e3729151b483ccad0372dc63e71bda298
svaddiAB <sukumar@auntbertha.com>
2017-05-02 06:34:49 -0500

    Changing Project settings

--- 1_Algorithmic toolbox/Week1/src/maximumPairWiseProduct/EculedianAlgorithmForGCD.java
+++ 1_Algorithmic toolbox/Week1/src/maximumPairWiseProduct/EculedianAlgorithmForGCD.java
@@ -1,17 +1,17 @@
 package maximumPairWiseProduct;
 
 public class EculedianAlgorithmForGCD {
-	public static int gcd(int a, int b){
-		if(b==0){
+	public static int gcd(int a, int b) {
+		if (b == 0) {
 			return a;
 		}
-		
-		return gcd( b, a%b);
-		
+
+		return gcd(b, a % b);
+
 	}
-	
-	public static void main(String[] args){
-		int result= gcd(357,234);
+
+	public static void main(String[] args) {
+		int result = gcd(357, 234);
 		System.out.println(result);
 	}
 }
