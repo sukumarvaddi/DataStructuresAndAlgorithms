a233ef28aa2b6634e4fca734ae19173cb9502fc6
chinnitpt <Sukumar.Vaddi@hhsc.state.tx.us>
2017-05-02 11:09:17 -0500

    Finished first problem of week3

--- 1_Algorithmic toolbox/Week3_greedy/src/Change.java
+++ 1_Algorithmic toolbox/Week3_greedy/src/Change.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Change {
 	private static int getChange(int m) {
-		return m;
+		int countForDenomination10 = m/10;
+		m = m-countForDenomination10*10;
+		int countForDenomination5 = m/5;
+		m = m-countForDenomination5*5;
+		int countForDenomination1= m/1;
+		return (countForDenomination1+countForDenomination10+countForDenomination5);
 	}
 
 	public static void main(String[] args) {
