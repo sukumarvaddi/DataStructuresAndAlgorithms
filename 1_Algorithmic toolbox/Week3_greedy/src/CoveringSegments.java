f84122bb764212125663b6c48218c17ca176e439
chinnitpt <Sukumar.Vaddi@hhsc.state.tx.us>
2017-05-03 17:12:27 -0500

    Finished Covering segments

--- 1_Algorithmic toolbox/Week3_greedy/src/CoveringSegments.java
+++ 1_Algorithmic toolbox/Week3_greedy/src/CoveringSegments.java
@@ -2,39 +2,84 @@ import java.util.*;
 
 public class CoveringSegments {
 
-    private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
-        return points;
-    }
+	private static int[] optimalPoints(Segment[] segments) {
+		// write your code here
+		Arrays.sort(segments);
+		List<Integer> points = new ArrayList<>();
+		int index = 0;
+		int i = index + 1;
+		Segment minRightMostPoint =null;
+		while (index < segments.length-1) {
+			minRightMostPoint = segments[index];
+			while (i < segments.length) {
+				Segment nextRightMostPoint = segments[i];
+				if (nextRightMostPoint.start <= minRightMostPoint.end) {
+					index = i + 1;
+					i = index;
+				} else if (nextRightMostPoint.start > minRightMostPoint.end) {
+					index = i;
+					i=(index+1);
+					points.add(minRightMostPoint.end);
+					break;
 
-    private static class Segment {
-        int start, end;
+				}
+				;
+			}
 
-        Segment(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        Segment[] segments = new Segment[n];
-        for (int i = 0; i < n; i++) {
-            int start, end;
-            start = scanner.nextInt();
-            end = scanner.nextInt();
-            segments[i] = new Segment(start, end);
-        }
-        int[] points = optimalPoints(segments);
-        System.out.println(points.length);
-        for (int point : points) {
-            System.out.print(point + " ");
-        }
-    }
+		}
+		
+		points.add(minRightMostPoint.end);
+		int[] pointsToReturn = new int[points.size()];
+		int j = 0;
+		for (Integer val : points) {
+			pointsToReturn[j] = val;
+			j++;
+		}
+		return pointsToReturn;
+	}
+
+	private static class Segment implements Comparable<Segment> {
+		int start, end;
+
+		Segment(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Segment o1) {
+			if (this.end < o1.end) {
+				return -1;
+			} else if (this.end == o1.end) {
+				if (this.start < o1.start) {
+					return -1;
+				} else if (this.start > o1.start) {
+					return 1;
+				} else {
+					return 0;
+				}
+
+			} else
+				return 1;
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		Segment[] segments = new Segment[n];
+		for (int i = 0; i < n; i++) {
+			int start, end;
+			start = scanner.nextInt();
+			end = scanner.nextInt();
+			segments[i] = new Segment(start, end);
+		}
+		int[] points = optimalPoints(segments);
+		System.out.println(points.length);
+		for (int point : points) {
+			System.out.print(point + " ");
+		}
+	}
 }
- 
